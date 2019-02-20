15137a70198c16e8232509bf1f43b5ba4aa3848f
alxbrg <kva.borg@gmail.com>
2019-02-20 16:42:09 +0100

    add IK1203 tasks

---
+++ KTH-projects/IK1203/task2/HTTPEcho.java
@@ -0,0 +1,27 @@
+import java.net.*;
+import java.io.*;
+
+public class HTTPEcho {
+    public static void main(String[] args) throws IOException{
+        StringBuilder serverResponse = new StringBuilder();
+
+        int port = Integer.parseInt(args[0]);
+        ServerSocket welcomeSocket = new ServerSocket(port);
+
+        while(true) {
+          Socket connectionSocket = welcomeSocket.accept();
+          BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+          DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+          outToClient.writeBytes("HTTP/1.1 200 OK\r\n\r\n");
+
+          String st;
+          while(!(st = inFromClient.readLine()).isEmpty()) {
+            serverResponse.append(st + "\r\n");
+          }
+
+          outToClient.writeBytes(serverResponse.toString());
+          connectionSocket.close();
+        }
+    }
+}
---
+++ KTH-projects/IK1203/task3/HTTPAsk.java
@@ -0,0 +1,81 @@
+import java.net.*;
+import java.io.*;
+
+public class HTTPAsk {
+    public static void main( String[] args) throws IOException {
+      String serverResponse;
+
+      int port = Integer.parseInt(args[0]);
+      ServerSocket welcomeSocket = new ServerSocket(port);
+
+      while(true) {
+        Socket connectionSocket = welcomeSocket.accept();
+        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+        DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+        try {
+          String url = getUrl(inFromClient);
+          String tcpHost = getHost(url);
+          int tcpPort = Integer.parseInt(getPort(url));
+          String tcpString = getString(url);
+
+          try {
+            serverResponse = ask(tcpHost, tcpPort, tcpString);
+            outToClient.writeBytes("HTTP/1.1 200 OK\r\n\r\n");
+            outToClient.writeBytes(serverResponse);
+          } catch (Exception e) {
+            outToClient.writeBytes("HTTP/1.1 404 Not Found\r\n\r\n");
+            connectionSocket.close();
+          }
+
+        } catch(Exception e) {
+          outToClient.writeBytes("HTTP/1.1 400 Bad Request\r\n\r\n");
+          connectionSocket.close();
+        }
+
+        connectionSocket.close();
+
+      }
+    }
+
+    private static String ask(String host, int port, String str) throws IOException {
+      TCPClient client = new TCPClient();
+      String response;
+
+      if (str.equals("")) {
+        response = client.askServer(host, port);
+        return response;
+      } else {
+        response = client.askServer(host, port, str);
+        return response;
+      }
+    }
+
+    private static String getUrl(BufferedReader br) throws IOException {
+      String httpReq = br.readLine();
+      String[] url = httpReq.split(" ");
+      return url[1];
+    }
+
+    private static String getHost(String url) {
+      String[] arr = url.split("[=&]");
+      String host = arr[1];
+      return host;
+    }
+
+    private static String getPort(String url) {
+      String[] arr = url.split("[=&]");
+      return arr[3];
+    }
+
+    private static String getString(String url) {
+      String[] arr = url.split("string=");
+      if (arr.length > 1) {
+        return arr[1];
+      } else {
+        return "";
+      }
+    }
+
+
+}
